6b1abe9166af48b7411be262e1070ca0b19a102a
teckytrill <jdjeuf@terpmail.umd.edu>
2025-03-26 17:45:09 -0400

    first try?

---
+++ src/Ingredient.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Ingredient(id, name){
+
+    <div class = "fav">
+        <h2>Favorite Ingredients</h2>   // should this link to something else
+    </div>
+        
+    
+
+}
